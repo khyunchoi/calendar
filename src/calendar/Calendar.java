49f2b9731885943aa0499e1a73dd6851675bff63
khyun <khyun@125.136.123.106>
2021-02-07 01:20:59 +0900

    달력 찍기1 클론

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,39 +1,24 @@
 package calendar;
 
-import java.util.Scanner;
-
 public class Calendar {
-	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private final static int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 	public int getMaxDaysOfMonth(int month) {
 		return MAX_DAYS[month - 1];
 	}
 
-	public void printSampleCalendar() {
-		System.out.println("일  월  화 수 목 금  토");
-		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-	}
+	public void printCalendar(int year, int month) {
+		System.out.printf("    <<%4d년%3d월>>\n", year, month);
+		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println("---------------------");
+		int maxday = getMaxDaysOfMonth(month);
 
-	public static void main(String[] args) {
-
-		String prompt = "> ";
-		Scanner scanner = new Scanner(System.in);
-		Calendar cal = new Calendar();
-		int month = 0;
-		while (true) {
-			System.out.println("월을 입력하세요.");
-			System.out.print(prompt);
-			month = scanner.nextInt();
-			if (month == -1) {
-				break;
+		for (int i = 1; i <= maxday; i++) {
+			System.out.printf("%3d", i);
+			if(i%7 == 0) {
+				System.out.println();
 			}
-			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
 		}
-		System.out.println("Bye");
-		scanner.close();
+		System.out.println();
 	}
 }
