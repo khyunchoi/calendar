cf4cd11aa25cc53302db14720c46833b3675b1c6
khyun <khyun@125.136.123.106>
2021-02-06 23:55:32 +0900

    update using class method

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -3,21 +3,26 @@ package calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	public static void main(String[] args) {
-		
+	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	public int getMaxDaysOfMonth(int i) {
+		return MAX_DAYS[i-1];
+	}
+	public void printSampleCalendar(){
 		System.out.println("일  월  화 수 목 금  토");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+	}
+	public static void main(String[] args) {
 		
-		int i;
-		int[] month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
 		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
 		System.out.println("달을 입력하세요.");
-		i = scanner.nextInt();
-		System.out.println(i+"월은 "+month[i-1]+"일까지 있습니다.");
+		int i = scanner.nextInt();
+		System.out.println(i+"월은 "+cal.getMaxDaysOfMonth(i)+"일까지 있습니다.");
 		scanner.close();
 	}
 }
