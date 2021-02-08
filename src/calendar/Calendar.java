d9a737e08f39633acf421a93cce59a69525a0dc2
khyun <khyun@125.136.123.106>
2021-02-08 22:52:39 +0900

    강의 마무리 공부가 많이 필요할듯

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,22 +1,55 @@
 package calendar;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Scanner;
 
 
 public class Calendar {
 	private final static int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private final static int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
+	private final static String SAVE_FILE="calendar.dat";
 	public HashMap<Date, PlanItem> planMap;
 	
 	public Calendar() {
 		planMap = new HashMap<Date, PlanItem>();
+		File f = new File(SAVE_FILE);
+		if (!f.exists()) {
+			return;
+		}
+		try {
+			Scanner s = new Scanner(f);
+			while(s.hasNext()) {
+				String line = s.nextLine();
+				String[] words = line.split(",");
+				String date = words[0];
+				String detail = words[1].replaceAll("\"", "");
+				PlanItem p = new PlanItem(date, detail);
+				planMap.put(p.getDate(), p);
+			}
+			s.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void registerPlan(String strDate, String plan) {
 		PlanItem p = new PlanItem(strDate, plan);
 		planMap.put(p.getDate(), p);
+		
+		File f = new File(SAVE_FILE);
+		String item = p.saveString();
+		try {
+			FileWriter fw = new FileWriter(f, true);
+			fw.write(item);
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public PlanItem searchPlan(String strDate) {
